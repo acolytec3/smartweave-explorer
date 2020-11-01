555c32f5d207dd796615e8e36bb99246b81a554f
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-10-31 21:13:10 -0400

    Click outside speeddial to close

--- src/components/SpeedDial.tsx
+++ src/components/SpeedDial.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaPlus, FaMinus, FaWallet } from 'react-icons/fa'
 import { motion, AnimatePresence } from 'framer-motion'
-import { IconButton, useToast } from '@chakra-ui/core'
+import { IconButton } from '@chakra-ui/core'
 import { del } from 'idb-keyval'
 import WalletContext from '../context/walletContext'
 
@@ -9,10 +9,25 @@ const MotionButton = motion.custom(IconButton)
 
 const SpeedDial = () => {
   const [isOpen, setIsOpen] = React.useState(false);
-  const {state, dispatch} = React.useContext(WalletContext)
-  const toast = useToast();
+  const { state, dispatch } = React.useContext(WalletContext)
+  const wrapperRef = React.useRef(null);
 
-  return (<React.Fragment>
+  const useOutsideAlerter = (ref: any) => {
+    React.useEffect(() => {
+      function handleClickOutside(event: MouseEvent) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setIsOpen(false)
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+  useOutsideAlerter(wrapperRef);
+
+  return (<div ref={wrapperRef}>
     <IconButton aria-label="open" isRound icon={isOpen ? FaMinus : FaPlus} position="fixed" bottom="20px" right="20px" onClick={() => setIsOpen(!isOpen)} />
     <AnimatePresence>
       {isOpen &&
@@ -29,9 +44,10 @@ const SpeedDial = () => {
           right="20px"
           onClick={async () => {
             await del('wallet')
-            dispatch({ type: 'ADD_WALLET', payload: {address:'',balance:'', key: '' }})
-            }} />}
-    </AnimatePresence></React.Fragment>)
+            dispatch({ type: 'ADD_WALLET', payload: { address: '', balance: '', key: '' } })
+          }} />}
+    </AnimatePresence>
+  </div>)
 }
 
 export default SpeedDial
