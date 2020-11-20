ecbe5e20fabe181c70bb753c1c346a161edc2418
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-11-19 21:16:11 -0500

    Implement buy logic and start of sell

--- src/components/VertoWidget.tsx
+++ src/components/VertoWidget.tsx
@@ -1,7 +1,7 @@
-import { Box, Center, Divider, Heading, Spinner, Text, VStack } from "@chakra-ui/core";
+import { Box, Button, Center, Divider, Heading, HStack, Input, Spinner, Text, VStack } from "@chakra-ui/core";
 import React from 'react';
 import WalletContext from '../context/walletContext';
-import { getOpenBuyDeets, getOpenSellDeets } from '../providers/verto';
+import { getOpenBuyDeets, getOpenSellDeets, trade } from '../providers/verto';
 
 interface VertoProps {
     contractID: string,
@@ -11,7 +11,12 @@ interface VertoProps {
 const VertoWidget: React.FC<VertoProps> = ({ contractID, ticker }) => {
     const [loading, setLoading] = React.useState(false)
     const [trades, setTrades] = React.useState({} as any)
+    const [purchaseAmount, setAmount] = React.useState('')
+    const [convertedAmount, setConversion] = React.useState(0)
+    const [maxBuy, setMax] = React.useState(0)
     const { state } = React.useContext(WalletContext)
+    const [sellAmount, setSell] = React.useState('')
+    const [sellConvertedAmount, setSellConversion] = React.useState(0)
 
     React.useEffect(() => {
         setLoading(true)
@@ -20,19 +25,77 @@ const VertoWidget: React.FC<VertoProps> = ({ contractID, ticker }) => {
             let sell = await getOpenSellDeets(contractID, state.key)
             await setTrades({ buy: buy, sell: sell })
             setLoading(false)
+            let txns = await trade("Buy", Math.floor(parseFloat(state.balance)), state.key, contractID);
+            setMax(txns.ar)
         }
         getTrades()
     }, [])
+
+    const handleBuy = async () => {
+        if (convertedAmount === 0) {
+            await calculateAr();
+        }
+        let res = await trade("Buy", convertedAmount, state.key, contractID)
+    }
+
+    const calculateAr = async () => {
+        let amount = parseInt(purchaseAmount)
+        if (!amount) {
+            setConversion(0)
+            return
+        }
+        else {
+            let ar = 0
+            for (let j = 0; j < trades.buy.rates.length; j++) {
+                if (trades.buy.rates[j].amount < amount) {
+                    ar += trades.buy.rates[j].amount * trades.buy.rates[j].rate
+                    amount -= trades.buy.rates[j].amount
+                }
+                else {
+                    ar += trades.buy.rates[j].rate * amount
+                    break;
+                }
+            }
+            setConversion(ar)
+        }
+    }
+
+    const handleSell = async () => {
+        let res = await trade("Sell", parseInt(sellAmount), state.key, contractID)
+    }
+    
     return (
         <VStack align="center">
             {loading && <Spinner />}
             {!loading && <Box>
                 <Heading size="sm">Buy/Sell {ticker}</Heading>
                 <Divider />
-                {trades.buy && trades.buy.volume > 0 ? <><Text>Average Buy Price: {trades.buy.averageRate.toLocaleString(undefined, { maximumFractionDigits: 6 })} {ticker}/AR</Text>
-                    <Text>Total available supply: {trades.buy.volume} {ticker}</Text></> : <Text>No traders are currently buying</Text>}
-                {trades.sell && trades.sell.volume > 0 ? <><Text>Average Sell Price: {trades.sell.averageRate.toLocaleString(undefined, { maximumFractionDigits: 6 })} {ticker}/AR</Text>
-                    <Text>Total of open orders: {trades.sell.volume} {ticker}</Text> </> : <Text>No traders are currently selling</Text>}
+                {trades.buy && trades.buy.volume > 0 ? <>
+                    <Text>Average Buy Price: {trades.buy.averageRate.toLocaleString(undefined, { maximumFractionDigits: 6 })} {ticker}/AR</Text>
+                    <Text>Total Available {ticker}: {trades.buy.volume}</Text>
+                    <Text>Total AR you can spend: {maxBuy}</Text>
+                    <HStack>
+                        <Input placeholder="Enter amount" value={purchaseAmount}
+                            onChange={((evt: React.ChangeEvent<HTMLInputElement>) => setAmount(evt.target.value))}
+                            onBlur={calculateAr} />
+                        <Text>Cost in AR: {convertedAmount}</Text>
+                        <Button onClick={handleBuy}>Buy</Button>
+                    </HStack>
+                </>
+                    :
+                    <Text>No open buy orders</Text>}
+                {trades.sell && trades.sell.volume > 0 ? <>
+                    <Text>Average Sell Price: {trades.sell.averageRate.toLocaleString(undefined, { maximumFractionDigits: 6 })} {ticker}/AR</Text>
+                    <Text>Total of open orders: {trades.sell.volume} {ticker}</Text>
+                    <HStack>
+                        <Input placeholder="Enter amount" value={sellAmount}
+                            onChange={((evt: React.ChangeEvent<HTMLInputElement>) => setSell(evt.target.value))}
+                            />
+                        <Text>Amount in AR: {sellConvertedAmount}</Text>
+                        <Button onClick={handleSell}>Sell</Button>
+                    </HStack>
+                    </>
+                    : <Text>No open sell orders</Text>}
             </Box>}
         </VStack>
     )
--- src/providers/verto.ts
+++ src/providers/verto.ts
@@ -1,30 +1,50 @@
 import Verto from '@verto/lib'
 import { getArweaveInstance } from './wallets'
+import { JWKInterface } from "arweave/node/lib/wallet";
 
 const getVerto = async (key: any) => {
     let arweave = getArweaveInstance()
     return new Verto(key)
 }
 
-export const getOpenBuyDeets = async (contractID : string, key: any) : Promise<{volume: number, averageRate: number}> => {
+export const getOpenBuyDeets = async (contractID: string, key: JWKInterface): Promise<{ volume: number, averageRate: number, rates: any[] }> => {
     let verto = await getVerto(key)
     let tPosts = await verto.getTradingPosts()
     let config = await verto.getConfig(tPosts[0])
-    let res = await (await (await fetch(`https://${config.publicURL}/orders`))).json()
-    let orders = res.find((token : any) => token.token === contractID ).orders.filter((order:any) => order.type === "Sell")
-    let totalOrders = orders.reduce((total: number, order:any) => total + order.amnt, 0)
-    let averageRate = orders.reduce((average: number, order:any) => average + (order.amnt/totalOrders)*order.rate,0)
-    return {volume: totalOrders, averageRate: 1/averageRate}
+    if (config.hasOwnProperty('publicURL')) {
+        //@ts-ignore
+        let res = await (await (await fetch(`https://${config.publicURL}/orders`))).json()
+        let orders = res.find((token: any) => token.token === contractID).orders.filter((order: any) => order.type === "Sell")
+        .sort((orderA:any,orderB:any) => orderB.rate - orderA.rate )
+        let totalOrders = orders.reduce((total: number, order: any) => total + order.amnt, 0)
+        let averageRate = orders.reduce((average: number, order: any) => average + (order.amnt / totalOrders) * order.rate, 0)
+        let individualRates = orders.map((order:any) => {return ({rate: 1/order.rate, amount: order.amnt})})
+        return { volume: totalOrders, averageRate: 1 / averageRate, rates: individualRates }
+    }
+    else return { volume: 0, averageRate: 0, rates:[] }
 }
 
-export const getOpenSellDeets = async (contractID : string, key: any) : Promise<{volume: number, averageRate: number}> => {
+export const getOpenSellDeets = async (contractID: string, key: JWKInterface): Promise<{ volume: number, averageRate: number, rates: any[] }> => {
     let verto = await getVerto(key)
     let tPosts = await verto.getTradingPosts()
     let config = await verto.getConfig(tPosts[0])
-    console.log(config)
-    let res = await (await (await fetch(`https://${config.publicURL}/orders`))).json()
-    let orders = res.find((token : any) => token.token === contractID ).orders.filter((order:any) => order.type === "Buy")
-    let totalOrders = orders.reduce((total: number, order:any) => total + order.amnt, 0)
-    let averageRate = orders.reduce((average: number, order:any) => average + (order.amnt/totalOrders)*order.rate,0)
-    return {volume: totalOrders, averageRate: averageRate}
+    if (config.hasOwnProperty('publicURL')) {
+        //@ts-ignore
+        let res = await (await (await fetch(`https://${config.publicURL}/orders`))).json()
+        let orders = res.find((token: any) => token.token === contractID).orders.filter((order: any) => order.type === "Buy")
+        .sort((orderA:any,orderB:any) => orderA.rate - orderB.rate )
+        let totalOrders = orders.reduce((total: number, order: any) => total + order.amnt, 0)
+        let averageRate = orders.reduce((average: number, order: any) => average + (order.amnt / totalOrders) * order.rate, 0)
+        let individualRates = orders.map((order:any) => {return ({rate: order.rate, amount: order.amnt})})
+        return { volume: totalOrders, averageRate: averageRate, rates: individualRates }
+    }
+    else return { volume: 0, averageRate: 0, rates: []}
+}
+
+export const trade = async (trade: string, amount: number, key: JWKInterface, contractID: string): Promise<any> => {
+    let verto = await getVerto(key)
+    let tPosts = await verto.getTradingPosts()
+    let txns = await verto.createOrder(trade, amount, contractID, tPosts[0])
+    console.log(txns)
+    return txns
 }
