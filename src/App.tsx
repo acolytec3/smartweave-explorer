769d11eb239754488638458f73d4a10d5ab55db3
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-02-20 19:27:47 -0500

    Remove the crap

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,24 @@
 import {
-  Box, Heading, Spinner, Stack, Tab, TabList, TabPanel, TabPanels, Tabs
+  Heading, Stack, Tab, TabList, TabPanel, TabPanels, Tabs
 } from '@chakra-ui/react';
-import { get } from 'idb-keyval';
+
 import React from 'react';
-import { FaCameraRetro, FaUpload } from 'react-icons/fa';
-import CameraWindow from './components/Camera';
 import SmartweaveExplorer from './components/Smartweave';
-import { SpeedDial, SpeedDialItem } from './components/SpeedDial';
-import Tokens from './components/Tokens';
-import TransactionDrawer from './components/TransactionDrawer';
-import Transactions from './components/Transactions';
 import WalletLoader from './components/WalletLoader';
 import WalletContext, { initWalletState } from './context/walletContext';
-import { getBlockHeight } from './providers/wallets';
 import walletReducer from './reducers/walletReducer';
 
 function App() {
   const [state, dispatch] = React.useReducer(walletReducer, initWalletState)
-  const [open, setOpen] = React.useState(false)
-  const [openCamera, setCamera] = React.useState(false)
-  const [loading, setLoading] = React.useState(true)
-
-  const handleClose = (modal: string) => {
-    if (modal === 'txn') setOpen(false);
-    if (modal === 'camera') setCamera(false)
-  }
-
-  // Retrieves block height for use in calculating approximate vaulted balance times for PSTs
-  React.useEffect(() => {
-    getBlockHeight().then((res) => dispatch({ type: 'SET_BLOCK_HEIGHT', payload: { blockHeight: res } }))
-  }, [])
-
-  React.useEffect(() => {
-    get('wallets').then((data: any) => {
-      if (data) {
-        let loadedState = JSON.parse(data)
-        dispatch({ type: 'LOAD_STATE', payload: { state: loadedState}})
-      }
-      setLoading(false)
-    })
-  }, [])
 
   return (
     <WalletContext.Provider value={{ dispatch, state }}>
-
         <Stack w="100%" align="center" >
           <Heading>ArMob 2.0</Heading>
           <Tabs isFitted align="center" variant="enclosed-colored">
             <TabPanels w="90vw">
               <TabPanel>
                 <WalletLoader />
-              </TabPanel>
-              <TabPanel>
-                {loading ? <Spinner position="fixed" bottom="50%" right="50%" /> :
-                  state.address !== '' &&
-                  <Box>
-                    <Tokens />
-                  </Box>
-                }
-              </TabPanel>
-              <TabPanel>
-                {state.address !== '' &&
-                  <Transactions />
-                }
               </TabPanel>
               <TabPanel>
                 <SmartweaveExplorer />
@@ -76,13 +32,6 @@ function App() {
             </TabList>
           </Tabs>
         </Stack>
-        {state.key && <SpeedDial>
-          {state.key && <SpeedDialItem key='faupload' icon={<FaUpload />} label="Upload File" clickHandler={() => setOpen(true)} />
-          }
-          {state.key && <SpeedDialItem key='facameretro' icon={<FaCameraRetro />} label="Take Picture" clickHandler={() => setCamera(true)} />}
-        </SpeedDial>}
-        <TransactionDrawer isOpen={open} close={() => handleClose('txn')} />
-        <CameraWindow isOpen={openCamera} close={() => handleClose('camera')} setTxnOpen={() => setOpen(true)} />
     </WalletContext.Provider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 
 ReactDOM.render(
@@ -20,7 +19,3 @@ ReactDOM.render(
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.unregister();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
